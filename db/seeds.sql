50a2add0776b2395a9e509282a3120c1b679b6b2
Mitchell Waite <mitchellwaite@Aubreys-MacBook-Pro.local>
2018-11-06 22:08:54 -0700

    Updated Homework

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pastrami Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Steak Burger', false);
+
